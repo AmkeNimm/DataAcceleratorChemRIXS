e18d73c44ac3d6bc6ec6bca5a38cb61fae67fbce
Amke Nimmrich <amke@sdfiana025.sdf.slac.stanford.edu>
2025-07-17 10:00:54 -0700

    testing data loading

--- loadDat.py
+++ loadDat.py
@@ -38,37 +38,81 @@ class HDF5Handling:
     Many attributes will not show up dynamically in an interpreter, because they are
     gotten dynamically from the file.
     """
-'''
-First level keys
- ['Sums', 'UserDataCfg', 'c_piranha', 'crix_w8', 'det_crix_w8', 'det_rix_fim0', 'det_rix_fim1', 'epics_archiver', 'intg', 'lightStatus', 'mono_hrencoder', 'rix_fim0', 'rix_fim1', 'scan', 'timestamp', 'timing']
+    '''
+    First level keys
+    ['Sums', 'UserDataCfg', 'c_piranha', 'crix_w8', 'det_crix_w8', 'det_rix_fim0', 'det_rix_fim1', 'epics_archiver', 'intg', 'lightStatus', 'mono_hrencoder', 'rix_fim0', 'rix_fim1', 'scan', 'timestamp', 'timing']
 
- Problem : THEY WILL CHANGE
- '''
+    Problem : THEY WILL CHANGE
+    '''
 
 
     
     _defaults = {}
     _string_attrs = frozenset({"xx"})
-    _int_attrs = frozenset({"xx"})
+    _int_attrs = frozenset({"andor_dir","andor_vls","axis_svls","timestamp"})
+    _ssdat_attrs = frozenset({"crix_w8","rix_fim0","rix_fim1","timing"})
     _float_attrs = frozenset({})
     _bool_attrs = frozenset({})
 
     def __init__(self, path: str | Path | h5py.File | h5py.Group):
         self.__file = None
 
-        if isinstance(path, h5py.File):
-            self.path = Path(path.filename).resolve()
-            self.__file = path
-            self.__intg = path["/intg"]
-            self.__singles = path["/Data"]
-        elif isinstance(path, h5py.Group):
-            self.path = Path(path.file.filename).resolve()
-            self.__file = path.file
-            self.__header = path
-            self.__datagrp = self.__file["/Data"]
-        elif isinstance(path, str | Path):
-            self.path = Path(path).resolve()
+        self.path = Path(path.filename).resolve()
+        self.__file= path
+        self.__ssdat = path["/"]
+        self.__intgrp = path["/intg"]
+
+    def is_open(self) -> bool:
+        """Whether the file is open."""
+        return bool(self.__file)
+
+    def __del__(self):
+        """Close the file when the object is deleted."""
+        if self.__file:
+            self.__file.close()
+
+    def close(self):
+        """Close the file."""
+        self.__intgrp = None
+        self.__ssgrp = None
+
+        # need to refresh these
+        with contextlib.suppress(AttributeError):
+            del self.ssdat
+
+        with contextlib.suppress(AttributeError):
+            del self.datagrp
+
+        if self.__file:
+            self.__file.close()
+        self.__file = None
+
+    def open(self):  # noqa: A003
+        """Open the file."""
+        if not self.__file:
+            self.__file = h5py.File(self.path, "r")
+            self.__intgrp = self.__file["/intg"]
+            self.__ssdat = self.__file["/"]
 
 
+    
+    @cached_property
+    def intgrp(self) -> h5py.Group:
+        """Get the integrated detector group."""
+        if not self.__file:
+            self.open()
+        return self.__intgrp
 
-
+    @cached_property
+    def ssgrp(self) -> h5py.Group:
+        """Get the single shot data."""
+        ssdat = {}
+        if not self.__file:
+            self.open()
+        for name in self._ssdat_attrs:
+            if name in self.__file:
+                print(name)
+                ssdat[name] = self.__file[name]
+        return ssdat
+    
+    
